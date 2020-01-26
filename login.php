724fe6d0592e92643f91133ede9352dd1705954a
LX5321 <alexanderwebsite5321@gmail.com>
2020-01-26 17:29:45 +0530

    Modified Login

--- login.php
+++ login.php
@@ -1,24 +1,39 @@
 <?php
-include("config.php");
+include("php/config.php");
 session_start();
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // username and password sent from form 
-    $myusername = mysqli_real_escape_string($db, $_POST['username']);
-    $mypassword = mysqli_real_escape_string($db, $_POST['password']);
-
-    $sql = "SELECT id FROM users WHERE username = '$myusername' and passcode = '$mypassword'";
+function check_doctor($db, $username, $password){
+    $sql = "SELECT id FROM doctors WHERE username = '$username' and passcode = '$password'";
     $result = mysqli_query($db, $sql);
     $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
     $active = $row['active'];
-
     $count = mysqli_num_rows($result);
-    // If result matched $myusername and $mypassword, table row must be 1 row
     if ($count == 1) {
-        //  session_register("myusername");
-        $_SESSION['login_user'] = $myusername;
+        $_SESSION['login_user'] = $username;
         header("location: welcome.php");
     } else {
         $error = "Your Login Name or Password is invalid";
+        echo($error);
     }
+}
+
+
+function check_patient($db, $username, $password){
+    $sql = "SELECT id FROM users WHERE username = '$username' and passcode = '$password'";
+    $result = mysqli_query($db, $sql);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $active = $row['active'];
+    $count = mysqli_num_rows($result);
+    if ($count == 1) {
+        $_SESSION['login_user'] = $username;
+        header("location: welcome.php");
+    } else {
+        check_doctor($db, $username, $password);
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $myusername = mysqli_real_escape_string($db, $_POST['username']);
+    $mypassword = mysqli_real_escape_string($db, $_POST['password']);
+    check_patient($db, $myusername, $mypassword);
 }
