967155ceeead7fa6a35dff0daacd07af3f4d5879
LX5321 <alexanderwebsite5321@gmail.com>
2020-01-31 21:33:40 +0530

    Removed comment blocks

--- index.php
+++ index.php
@@ -6,11 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
   <meta http-equiv="x-ua-compatible" content="ie=edge" />
   <title>PredictorUI</title>
-  <!-- Bootstrap core CSS -->
   <link href="css/bootstrap.min.css" rel="stylesheet" />
-  <!-- Material Design Bootstrap -->
   <link href="css/mdb.min.css" rel="stylesheet" />
-  <!-- Your custom styles (optional) -->
   <link href="css/style.min.css" rel="stylesheet" />
   <style type="text/css">
     html,
@@ -49,41 +46,27 @@
 </head>
 
 <body>
-  <!-- Navbar -->
   <nav class="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar">
     <div class="container">
-      <!-- Brand -->
       <a class="navbar-brand" href="" target="_blank">
         <strong>PredictorUI</strong>
       </a>
-
-      <!-- Links -->
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <!-- Left -->
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active"></li>
         </ul>
       </div>
     </div>
   </nav>
-  <!-- Navbar -->
-
-  <!-- Full Page Intro -->
   <div class="view full-page-intro" style="background-image: url('img/sample.jpg'); background-repeat: no-repeat; background-size: cover;">
-    <!-- Mask & flexbox options-->
     <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
-      <!-- Content -->
       <div class="container">
-        <!--Grid row-->
         <div class="row wow fadeIn">
-          <!--Grid column-->
           <div class="col-md-6 mb-4 white-text text-center text-md-left">
             <h1 class="display-4 font-weight-bold">
               Lightweight User Interface for the backend.
             </h1>
-
             <hr class="hr-light" />
-
             <p>
               <strong>Now for the web.</strong>
             </p>
@@ -98,22 +81,14 @@
               See How it Works
             </a>
           </div>
-          <!--Grid column-->
-
-          <!--Grid column-->
           <div class="col-md-6 col-xl-5 mb-4">
-            <!--Card-->
             <div class="card">
-              <!--Card content-->
               <div class="card-body">
-                <!-- Form -->
                 <form name="" action="login.php" method="post">
-                  <!-- Heading -->
                   <h3 class="dark-grey-text text-center">
                     <strong>Welcome Back!</strong>
                   </h3>
                   <hr />
-
                   <div class="md-form">
                     <input type="text" name="username" id="username" class="form-control" />
                     <label for="form3">Username</label>
@@ -122,34 +97,22 @@
                     <input type="password" name="password" id="password" class="form-control" />
                     <label for="form2">Password</label>
                   </div>
-
                   <div class="text-center">
                     <button class="btn btn-indigo">Log In</button>
                     <hr />
                   </div>
-
                 </form>
-                <!-- Form -->
               </div>
             </div>
-            <!--/.Card-->
           </div>
-          <!--Grid column-->
         </div>
-        <!--Grid row-->
       </div>
-      <!-- Content -->
     </div>
-    <!-- Mask & flexbox options-->
   </div>
-  <!-- Full Page Intro -->
   <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
-  <!-- Bootstrap tooltips -->
   <script type="text/javascript" src="js/popper.min.js"></script>
-  <!-- Bootstrap core JavaScript -->
   <script type="text/javascript" src="js/bootstrap.min.js"></script>
-  <!-- MDB core JavaScript -->
   <script type="text/javascript" src="js/mdb.min.js"></script>
-  
 </body>
+
 </html>
