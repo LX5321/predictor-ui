04d09728f80d3f2d58c3b7b5fa26a835ecd735a2
LX5321 <alexanderwebsite5321@gmail.com>
2020-02-10 08:32:14 +0530

    Added avatar include

--- php/header.php
+++ php/header.php
@@ -6,6 +6,16 @@ if (!isset($username)) {
     header("location: ../php/403.html");
 }
 ?>
-<html><nav class="navbar navbar-expand-lg navbar-dark primary-color">
-        <a class="navbar-brand" href="index.php">PredictorUI</a>
-    </nav></html>
+<html>
+    <nav class="navbar navbar-expand-lg navbar-dark primary-color">
+    <a class="navbar-brand" href="index.php">PredictorUI</a>
+    <ul class="navbar-nav ml-auto nav-flex-icons">
+        <li class="nav-item avatar">
+            <a class="nav-link p-0" href="#">
+                <img src="avatar-5.jpg" class="rounded-circle z-depth-0" alt="avatar image" height="45">
+            </a>
+        </li>
+    </ul>
+</nav>
+
+</html>
