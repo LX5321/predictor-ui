c3a538dbd48bcbabc6de2bbf3919a17d25068c61
LX5321 <alexanderwebsite5321@gmail.com>
2020-02-12 07:54:04 +0530

    Added Working SQL graph query

--- patient/index.php
+++ patient/index.php
@@ -8,20 +8,19 @@ if (!isset($username)) {
     header("location: ../php/403.html");
 }
 
+$glucose = [];
+
 $sql = "SELECT * FROM diagnosis WHERE diagnosis.user_id = $user_id  ORDER by diagnosis.id DESC LIMIT 20";
 if ($result = mysqli_query($db, $sql)) {
     if (mysqli_num_rows($result) > 0) {
         while ($row = mysqli_fetch_array($result)) {
-            echo $row['Glucose']."\n";
+            array_push($glucose, $row['Glucose']);
         }
         mysqli_free_result($result);
     }
 } else {
     echo "";
 }
-
-
-
 
 
 $sql = "SELECT id FROM users where username=\"$username\";";
@@ -60,6 +59,12 @@ if ($result = mysqli_query($db, $sql)) {
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($db);
 }
 
+$Array = array(21, 4, 43, 56, 44, 5, 72, 13);
+$Array2 = array(1, 2, 42, 1, 42, 53, 3, -63, 2);
+$Array3 = array(4, 5, 2, 14, 25, 46, 86, 52, 2, 2);
+$List = implode(', ', $glucose);
+$List2 = implode(', ', $Array2);
+$List3 = implode(', ', $Array3);
 // Close connection
 mysqli_close($db);
 ?>
@@ -95,37 +100,96 @@ mysqli_close($db);
         </div>
     </div>
 
-    <div class="sm-4">
-        <div class="container">
-            <div class="card-deck">
-                <div class="card mb-4">
-                    <div class="card-body">
-                        <h4 class="card-title">Glucose</h4>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <button type="button" class="btn btn-light-blue btn-md">Read more</button>
+
+    <div class="container sm-4">
+        <div class="card-deck">
+            <div class="card mb-4">
+                <div class="view overlay">
+                    <div>
+                        <canvas id="lineChart"></canvas>
                     </div>
                 </div>
-                <div class="card mb-4">
-                    <div class="card-body">
-                        <h4 class="card-title">Insulin</h4>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <button type="button" class="btn btn-light-blue btn-md">Read more</button>
+                <div class="card-body">
+                    <h4 class="card-title">Glucose</h4>
+                </div>
+            </div>
+            <div class="card mb-4">
+                <div class="view overlay">
+                    <div>
+                        <canvas id="lineChart2"></canvas>
                     </div>
                 </div>
-                <div class="card mb-4">
-                    <div class="card-body">
-                        <h4 class="card-title">Body Mass Index</h4>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <button type="button" class="btn btn-light-blue btn-md">Read more</button>
+                <div class="card-body">
+                    <h4 class="card-title">Insulin</h4>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <button type="button" class="btn btn-light-blue btn-md">Read more</button>
+                </div>
+            </div>
+            <div class="card mb-4">
+                <div class="view overlay">
+                    <div>
+                        <canvas id="lineChart3"></canvas>
                     </div>
+                </div>
+                <div class="card-body">
+                    <h4 class="card-title">Body Mass Index</h4>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <button type="button" class="btn btn-light-blue btn-md">Read more</button>
                 </div>
             </div>
         </div>
     </div>
+
+
     <script type="text/javascript" src="../js/jquery-3.4.1.min.js"></script>
     <script type="text/javascript" src="../js/popper.min.js"></script>
     <script type="text/javascript" src="../js/bootstrap.min.js"></script>
     <script type="text/javascript" src="../js/mdb.min.js"></script>
+
+    <script>
+        var ctxL = document.getElementById("lineChart").getContext('2d');
+        var myLineChart = new Chart(ctxL, {
+            type: 'line',
+            data: {
+                labels: [<?php print_r($List); ?>],
+                datasets: [{
+                    data: [<?php print_r($List); ?>],
+                    backgroundColor: [
+                        'rgba(0, 250, 132, .2)'
+                    ],
+                    borderColor: [
+                        'rgba(0, 10, 130, .7)',
+                    ],
+                    borderWidth: 2
+                }]
+            },
+
+            options: {
+                responsive: true,
+                scales: {
+                    xAxes: [{
+                        display: false
+                    }],
+                    yAxes: [{
+                        display: false
+                    }]
+                },
+                legend: {
+                    display: false
+                },
+                tooltips: {
+                    enabled: false
+                },
+                elements: {
+                    point: {
+                        radius: 0
+                    }
+                }
+
+            }
+        });
+    </script>
+
 </body>
 
 </html>
