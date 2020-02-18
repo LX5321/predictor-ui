94c8e850fa854b915d1fd9a92f78a0008531b0c9
LX5321 <alexanderwebsite5321@gmail.com>
2020-02-18 20:47:56 +0530

    Added Basic Template

---
+++ doctor/insertnew.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>Home | Predictor</title>
+    <link href="../css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../css/mdb.min.css" rel="stylesheet" />
+    <link href="../css/style.css" rel="stylesheet" />
+
+</head>
+<?php
+include('../php/header.php');
+?>
+<div class="mb-2"></div>
+<div class="container">
+    <form class="p-5">
+
+        <p class="h4 mb-4 text-center">Sign in</p>
+        <div class="row">
+            <div class="col-sm">
+                <input type="email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="E-mail">
+            </div>
+            <div class="col-sm">
+                <input type="email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="E-mail">
+            </div>
+            <div class="col-sm">
+                <input type="email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="E-mail">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm">
+                <input type="email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="E-mail">
+            </div>
+            <div class="col-sm">
+                <input type="email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="E-mail">
+            </div>
+            <div class="col-sm">
+                <input type="email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="E-mail">
+            </div>
+        </div>
+
+        <button class="btn btn-secondary btn-block my-4" type="submit">Sign in</button>
+    </form>
+</div>
+
+<script type="text/javascript" src="../js/jquery-3.4.1.min.js"></script>
+<script type="text/javascript" src="../js/popper.min.js"></script>
+<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../js/mdb.min.js"></script>
+</body>
+
+</html>
